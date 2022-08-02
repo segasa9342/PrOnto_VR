397f3d413fc4af2f839d38935e26975026103f5d
Salvo <salvosapie@gmail.com>
2022-08-02 19:37:23 +0200

    Added Handtracking Fixed console.log innerHtml

--- component.js
+++ component.js
@@ -3,7 +3,6 @@ AFRAME.registerComponent('showbutton', {
     'target': {type: 'selector'},
   },
   init: function () {
-    console.log("registered");
     var show=false;
     this.el.addEventListener("click",()=>{
     var origin=this.el.components.showbutton.attrValue.target;
@@ -45,10 +44,37 @@ function AknLink() {
     request.open("GET", "../privacy_policy.xml", false);
     request.send();
     var xml = request.responseXML;
-    var chapters = xml.getElementsByTagName("chapter");
-    const chapter = Array.from(chapters).filter(chapter => chapter.innerHTML.indexOf(slideID) !== -1)
-    console.log(slideID, chapter[0])
+    if (slideID.includes("para")) {
+        var paragraphs = xml.getElementsByTagName("paragraph");
+        const paragraph = Array.from(paragraphs).filter(paragraph => paragraph.innerHTML.indexOf(slideID) !== -1);
+        return paragraph[0];
+    } else {
+        var chapters = xml.getElementsByTagName("chapter");
+        const chapter = Array.from(chapters).filter(chapter => chapter.innerHTML.indexOf(slideID) !== -1);
+        return chapter[0];
+    };
 };
+AFRAME.registerComponent("aknlink_log_component", {
+    init: function() {
+      aknSnippet = AknLink()
+      var show=false;
+        this.el.addEventListener("click", (e) => {
+        // create an <a-entity>
+            var txt = document.createElement('a-entity')
+            // use the mixin
+            txt.setAttribute("mixin", "text")
+            txt.setAttribute("visible", "true")
+            // set the text value
+            txt.setAttribute("text", "value", aknSnippet.innerHTML)
+            txt.setAttribute("position","2  2 -2")
+            this.el.appendChild(txt)
+            })
+      }
+    });
+
+function AknLink_log() {
+    console.log(AknLink())
+}
 
 function boxesdisappear() {
 document.querySelectorAll('a-box').forEach( x=> x.setAttribute("visible","false"));
