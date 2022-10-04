9d7453bd25c702f10cd09eeeba892dfdbc7b4b99
Salvo <salvosapie@gmail.com>
2022-10-04 11:12:43 +0200

    Fixing Cursors

--- component.js
+++ component.js
@@ -94,16 +94,7 @@ function multifunction(value) {
     boxesdisappear();
     showIcon(value)
 };
-document.querySelector('#blockHand').addEventListener(`click`, function (evt) {
-    // Create a blank entity.
-    var newVoxelEl = document.createElement('a-entity');
-
-    // Use the mixin to make it a voxel.
-    newVoxelEl.setAttribute('mixin', 'voxel');
-
-    // Set the position using intersection point. The `snap` component above which
-    // is part of the mixin will snap it to the closest half meter.
-    newVoxelEl.setAttribute('position', evt.detail.intersection.point);
-
-    // Add to the scene with `appendChild`. this.appendChild(newVoxelEl);
+var hand = document.getElementById("left-hand");
+hand.addEventListener("hand-tracking-extras-ready", (evt) = {
+  var jointsAPI = evt.detail.data.jointsAPI;
 });
